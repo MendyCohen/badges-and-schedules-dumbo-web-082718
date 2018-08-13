21302cc433d59b509f4788bb8b14165f5e612edc
Mendy <mendybeltcohen@gmail.com>
2018-08-13 00:22:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,5 +23,5 @@ def assign_rooms(name)
 end
 
 def printer(name)
-  puts "Hello, my name is #{batch_badge_creator(name[0])}. #{assign_rooms(name)}"
+  puts "Hello, my name is #{batch_badge_creator(name)}."
 end
